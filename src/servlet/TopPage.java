11e05789a319c685fba694aa825ef8b52bfc984c
step <step@STRA-CL0151.step.co.jp>
2020-07-01 13:55:22 +0900

    トップページのサーブレット

---
+++ src/servlet/TopPage.java
@@ -0,0 +1,65 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import common.DBClass;
+
+/**
+ * Servlet implementation class TopPage
+ */
+@WebServlet("/TopPage")
+public class TopPage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TopPage() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+
+		request.setCharacterEncoding("UTF-8");
+
+		HttpSession session=request.getSession();
+
+		DBClass db = new DBClass();
+
+		String Pass1 = request.getParameter("gPass");
+
+		db.dbOpen();
+
+		ArrayList<String[]> aryRanking5 = db.getRankingData(Pass1);
+
+		session.setAttribute("reqRanking", aryRanking5);
+
+
+		db.dbClose();
+
+		response.sendRedirect("jsp/TopPage.jsp");
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
