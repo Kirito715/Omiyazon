9be1097a26b5b2ae84a2786a73a3667eee44165f
ccks4 <ccks4@192.168.10.12>
2020-07-06 10:33:14 +0900

    sa

---
+++ src/servlet/Mypage.java
@@ -0,0 +1,62 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import common.DBClass;
+
+/**
+ * Servlet implementation class Mypage
+ */
+@WebServlet("/Mypage")
+public class Mypage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Mypage() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		request.setCharacterEncoding("UTF-8");
+		HttpSession session = request.getSession();
+		DBClass db = new DBClass();
+		String uid=request.getParameter("userid");
+		uid="1";
+		db.dbOpen();
+		ArrayList<String[]> a = db.getuser(uid);
+		ArrayList<String[]> b = db.getrecode(uid);
+		ArrayList<String[]> c = db.getbook(uid);
+		db.dbClose();
+		session.setAttribute("myuser",a);
+		session.setAttribute("myrecode",b);
+		session.setAttribute("mybook",c);
+		session.setAttribute("uid",uid);
+
+
+		response.sendRedirect("jsp/Mypage.jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
