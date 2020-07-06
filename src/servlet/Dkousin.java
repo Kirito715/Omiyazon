b547f19aa6cc986f19786d33bedfc31fb6ab62de
step <step@STRA-CL0151.step.co.jp>
2020-07-06 12:00:32 +0900

    追加

--- src/servlet/Dkousin.java
+++ src/servlet/Dkousin.java
@@ -1,4 +1,4 @@
-package servret;
+package servlet;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import comon.DBClass;
+import common.DBClass;
 
 /**
  * Servlet implementation class Dkousin
