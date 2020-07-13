cf4b21d8b5a972821246483b8cb5b51951cd6b66
東谷 <tr06.gameovermas@gmail.com>
2020-07-13 16:28:19 +0900

    a

--- src/common/DBClass.java
+++ src/common/DBClass.java
@@ -2094,9 +2094,15 @@ public class DBClass {
 			stmt = objCon.createStatement();
 
 	        String sql = "";
-	        sql += " SELECT 伝票ID,登録者ID,CONVERT(date, 購入日時) as 購入日時2,注文状態,発送日時";
-	        sql += " FROM  伝票マスタ";
-	        sql += " order by 購入日時 desc";
+//	        sql += " SELECT 伝票ID,登録者ID,CONVERT(date, 購入日時) as 購入日時2,注文状態,発送日時";
+//	        sql += " FROM  伝票マスタ";
+//	        sql += " order by 購入日時 desc";
+
+	        sql += " SELECT a.伝票ID,a.登録者ID,CONVERT(date, 購入日時) as 購入日時2,注文状態,発送日時";
+	        sql += " FROM  伝票マスタ as a inner join 売上明細マスタ as b on a.伝票ID=b.伝票ID";
+	        sql += " inner join 商品マスタ as c on b.商品ID=c.商品ID";
+	        sql +=" group by a.伝票ID,a.登録者ID,購入日時,注文状態,発送日時";
+        	sql += " order by 購入日時";
 
 	        // 実行SQL確認
 	        System.out.println(sql);
